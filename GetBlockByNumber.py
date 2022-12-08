49c4c80b2d0ddc181b97482ce3efb50643d96ce3
Ether1oop <1536622592@qq.com>
2022-12-08 21:43:58 +0800

    Initial commit

--- GetBlockByNumber.py
+++ GetBlockByNumber.py
@@ -9,7 +9,7 @@ latest_block_num = 16138610 # 2022.12.8
 # noinspection PyBroadException
 def getMessageFromBlock(block_num):
     result = ""
-    url = "https://api-cn.etherscan.com/api?module=proxy&action=eth_getBlockByNumber&tag="+ str(hex(block_num)) +"&boolean=true&apikey=NSDRFWGWMT6UUDEF44Y4HEF61QRU76WNGM"
+    url = "https://api-cn.etherscan.com/api?module=proxy&action=eth_getBlockByNumber&tag={}&boolean=true&apikey=NSDRFWGWMT6UUDEF44Y4HEF61QRU76WNGM".format(str(hex(block_num)))
     headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
     try:
         req = Request(url, headers=headers)
@@ -34,8 +34,16 @@ def readLastBlockNum():
     return last_block_num
 
 
+def _readLastBlockNum():
+    last_block_num = OpenFile.openFileByString("config/last_scapy_block_num.txt").strip()
+    if last_block_num == "":
+        return latest_block_num + 1
+    else:
+        return int(last_block_num)
+
+
 def ScapyBlockData():
-    last_block_num = readLastBlockNum()
+    last_block_num = _readLastBlockNum()
 
     while True:
         last_block_num -= 1
@@ -49,5 +57,6 @@ def ScapyBlockData():
 
         print("downloading " + str(last_block_num) + " block data")
         OpenFile.writeFile("block_data/" + str(last_block_num) + ".json", json.dumps(data))
+        OpenFile.writeFile("config/last_scapy_block_num.txt", str(last_block_num))
 
 
--- GetTransactionCode.py
+++ GetTransactionCode.py
@@ -1,8 +1,16 @@
 import json
+import os.path
+from urllib.request import Request, urlopen
 import OpenFile
 
 
+latest_block_num = 16138610 # 2022.12.8
+
+
 def getTransactionAddressAndInput(block_num):
+    if not os.path.exists("block_data/" + str(block_num) + ".json"):
+        return False
+
     block_str = OpenFile.openFileByString("block_data/" + str(block_num) + ".json")
     if block_str == "":
         print("Read File Error !")
@@ -16,17 +24,47 @@ def getTransactionAddressAndInput(block_num):
     for transaction in transaction_list:
         _address = transaction['to']
         _input = transaction['input']
-        if _input == "0x":
+        if _input == "0x" or _address is None:
             continue
         # 以合约地址为名创建文件，其内存放输入信息
         OpenFile.appendFile("inputs/" + _address + ".txt", _input + "\n")
-
-    OpenFile.writeFile("config/last_parse_block_num.txt", str(last_block_num - 1))
+    print("parsing block num :" + str(block_num))
+    OpenFile.writeFile("config/last_parse_block_num.txt", str(block_num))
     return True
+
+
+# noinspection PyBroadException
+def getTransactionCode(address):
+    result = ""
+    url = "https://api.etherscan.io/api?module=contract&action=getsourcecode&address={address}&apikey=NSDRFWGWMT6UUDEF44Y4HEF61QRU76WNGM".format(address=address)
+    headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+    try:
+        req = Request(url, headers=headers)
+        response = urlopen(req).read()
+        result = json.loads(response.decode())
+    except Exception:
+        print(Exception)
+    return result
+
+
+def parseTransactionAddressAndInput():
+    last_parse_block_num = OpenFile.openFileByString("config/last_parse_block_num.txt").strip()
+
+    if last_parse_block_num == "":
+        last_parse_block_num = latest_block_num + 1
+    else:
+        last_parse_block_num = int(last_parse_block_num)
+
+    while True:
+        flag = getTransactionAddressAndInput(last_parse_block_num - 1)
+        last_parse_block_num -= 1
+        if not flag:
+            break
+
+
+# def scapyTransactionCode():
 
 
 
 if __name__ == "__main__":
-    last_block_num = int(OpenFile.openFileByString("config/last_parse_block_num.txt").strip())
-
-    getTransactionAddressAndInput(last_block_num)
+    parseTransactionAddressAndInput()
